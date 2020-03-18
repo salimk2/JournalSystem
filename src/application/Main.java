5d80561fbd413e16d27c5ad5232e70b93ea2b6c5
Salim <salimkassouri2000@gmail.com>
2020-03-17 18:47:37 -0600

    edited Editor Design

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,10 @@
 package application;
 
-
-
-
-
 import javafx.application.Application;
 import javafx.stage.Stage;
-
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -21,14 +17,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		
-		
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
+			//AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("EditorV1.fxml"));
 			Scene scene = new Scene(root,800,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			Main.stage = primaryStage;
-			
 			
 			//Add icon to Main Page
 			Image icon =new Image("/images/Icons/icon1.png");
--- src/application/controllers/EditorV1Controller.java
+++ src/application/controllers/EditorV1Controller.java
@@ -1,8 +1,60 @@
 package application.controllers;
 
+import application.EditorRecord;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 /**
  * Controller class for EditorV1.fxml
  */
-public class EditorV1Controller {
+public class EditorV1Controller implements Initializable {
+	
+	// configure table
+	@FXML private TableView<EditorRecord> tableView;
+	@FXML private TableColumn<EditorRecord, String> submissionColumn;
+	@FXML private TableColumn<EditorRecord, String> reviewerColumn;
+	@FXML private TableColumn<EditorRecord, LocalDate> minorRevisionColumn;
+	@FXML private TableColumn<EditorRecord, LocalDate> revision1Column;
+	@FXML private TableColumn<EditorRecord, LocalDate> revision2Column;	
+	
+	/**
+	 * Initialize the controller class
+	 */
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+		// set columns
+		submissionColumn.setCellValueFactory(new PropertyValueFactory<EditorRecord, String>("submission"));  // instance variable to look for: submission
+		reviewerColumn.setCellValueFactory(new PropertyValueFactory<EditorRecord, String>("reviewer"));
+		minorRevisionColumn.setCellValueFactory(new PropertyValueFactory<EditorRecord, LocalDate>("minorRevision"));
+		revision1Column.setCellValueFactory(new PropertyValueFactory<EditorRecord, LocalDate>("revision1"));
+		revision2Column.setCellValueFactory(new PropertyValueFactory<EditorRecord, LocalDate>("revision2"));
+		
+		// load dummy data
+		tableView.setItems(getRecords());
+	}
+	
+	/**
+	 * this method will return an ObersableList of People objects 
+	 */
+	public ObservableList<EditorRecord> getRecords() {
+		
+		// ObservaleList behaves like an ArrayList with feature for GUI environment
+		ObservableList<EditorRecord> records = FXCollections.observableArrayList();
+		records.add(new EditorRecord("Submission1.pdf","Reviewer 1",LocalDate.of(2020, Month.DECEMBER, 12), LocalDate.of(2020, Month.DECEMBER, 20), LocalDate.of(2020, Month.DECEMBER, 28)));
+		records.add(new EditorRecord("Submission1.pdf","Reviewer 1",LocalDate.of(2020, Month.DECEMBER, 12), LocalDate.of(2020, Month.DECEMBER, 20), LocalDate.of(2020, Month.DECEMBER, 28)));
+		records.add(new EditorRecord("Submission1.pdf","Reviewer 1",LocalDate.of(2020, Month.DECEMBER, 12), LocalDate.of(2020, Month.DECEMBER, 20), LocalDate.of(2020, Month.DECEMBER, 28)));
+		
+		return records;	
+	}
 
 }
