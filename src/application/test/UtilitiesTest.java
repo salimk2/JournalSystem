87554eb22c73925b92abab013f67784d324aeb2c
Salim <salimkassouri2000@gmail.com>
2020-04-11 17:13:46 -0600

    Updated JUnit Tests

---
+++ src/application/test/UtilitiesTest.java
@@ -0,0 +1,14 @@
+package application.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UtilitiesTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
