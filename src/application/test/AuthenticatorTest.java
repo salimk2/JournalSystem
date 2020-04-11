d00e1823c076dec8d24f6270baa8bb0cc2e3cc27
Salim <salimkassouri2000@gmail.com>
2020-04-11 17:05:33 -0600

    Updated JUnit Tests

---
+++ src/application/test/AuthenticatorTest.java
@@ -0,0 +1,49 @@
+package application.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import application.Authenticator;
+
+class AuthenticatorTest {
+
+	@Test
+	void testAuthenticator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRegister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReadData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSubmissions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetReviews() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetComments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetWithdrawStatus() {
+		fail("Not yet implemented");
+	}
+
+}
